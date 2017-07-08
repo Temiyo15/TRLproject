90f4470ab31b518865fc1d3df1a17cebd8c80285
AlqahhasYazeed <eng.yazeed.92@gmail.com>
2017-07-08 15:37:39 -0500

    vc

---
+++ src/CopyTest.java
@@ -0,0 +1,109 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CopyTest {
+
+	@Test
+	public void testToString() {
+		
+		
+		
+	}
+
+	@Test
+	public void testCopyTextbookString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCopyStringTextbookPatronBooleanBooleanBooleanLocalDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testResetDue_date() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckIfOverdue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsChecked_out() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetChecked_out() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsOverdue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetOverdue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasHold() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetHold() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCheck_out_date() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCheck_out_date() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCopyID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCopyID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOutTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOutToID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetOutTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+}
